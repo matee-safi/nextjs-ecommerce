851d32eed2bb42f060f75b19aeb7151437c08242
matee-safi <matiullahsafibbc@gmail.com>
2025-02-03 09:26:51 +0430

    intit project

---
+++ app/components/DemoBanner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DemoBanner() {
+  return (
+    <div className="demo-banner-container">
+      <span>
+        This is a demo store - no orders will be accepted or delivered
+      </span>
+    </div>
+  );
+}
---
+++ app/components/NavBar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+// import { Cart } from "./";
+// import { useStateContext } from "../../context/StateContext";
+
+export default function NavBar() {
+  return (
+    <div className="navbar-container">
+      <div className="company-name">
+        <Link href="/">Mangez Macaron</Link>
+
+        <div className="navbar">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/shop">Shop</Link>
+          <Link href="/contact">Contact</Link>
+
+          {/* <button
+  type="button"
+  className="cart-icon"
+  onClick={() => setShowCart(true)}
+  >
+  <AiOutlineShopping />
+  <span className="cart-item-qty">{totalQuantities}</span>
+  </button>
+  {showCart && <Cart />} */}
+        </div>
+      </div>
+    </div>
+  );
+}
