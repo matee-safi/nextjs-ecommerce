7a5b67b884ff64b33eea2acaca0f93d6d6bc116c
matee-safi <matiullahsafibbc@gmail.com>
2025-02-06 14:09:24 +0430

    uninstall sanity vision

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import product from './schemas/product'
 
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
   dataset: process.env.SANITY_STUDIO_API_DATASET!,
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool()],
 
   schema: {
     types: [product],
